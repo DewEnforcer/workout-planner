aa9655823e7b8dd8365c04eeed4e1c878017b1de
DewEnforcer <piranha863@gmail.com>
2021-05-04 15:06:32 +0200

    Settings screen UI finished

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import AppForm from '../components/forms/Form';
+import AppFormField from '../components/forms/FormField';
+import SubmitButton from '../components/forms/SubmitButton';
 import Screen from '../components/Screen';
 
+import defaultStyles from "../config/styles";
+
 export default function SettingsScreen() {
+
+    const [weight, setWeight] = useState();
+
+    useEffect(() => {
+        console.log("Getting weight from local storage");
+    })
+
+    const handleSaveSettings = data => {
+        console.log("Saving settings", data);
+    }
+
     return (
         <Screen>
-            <Text>Hello world</Text>
+            <Text style={defaultStyles.headerText}>Settings</Text>
+            <AppForm
+                initialValues={{
+                    weight: weight
+                }}
+                onSubmit={handleSaveSettings}
+            >
+                <AppFormField name="weight" placeholder="Enter your weight..."/>
+                <SubmitButton title="Save"/>
+            </AppForm>
         </Screen>
     );
 }
